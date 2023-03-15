2de9816ee3f5734d08c536487ef14d09d4184c76
tehi_kim <k.tehi21@gmail.com>
2023-03-15 01:30:01 +0000

    loading spinner

---
+++ spinner1.js
@@ -0,0 +1,23 @@
+process.stdout.write('abcdefg good hello from spinner1.js... \rheyyy\n');
+
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+  console.log(" ");
+}, 700);
+
+setTimeout(() => {
+  console.log("Download complete");
+}, 800);
